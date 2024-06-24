8c36c2479271ff61e17382c83e6ce5ae97b33a8a
melf <mel.f@bestgame-co.com>
2024-06-24 15:12:23 +0800

    commented alert data

--- drag_and_drop/asset/js/script.js
+++ drag_and_drop/asset/js/script.js
@@ -7,8 +7,10 @@
 // ========================================================================
 //  JSON.stringify(value[, replacer[, space]])
 // ========================================================================
+/*
 var mydata = JSON.parse(data);
 alert(mydata[0].name);
 alert(mydata[0].age);
 alert(mydata[1].name);
 alert(mydata[1].age);
+*/
